d038ac4dc646ad77e809f35a5b2aaef0d89865a6
Harsh Yadav <120951112+Harshiitmadras@users.noreply.github.com>
2025-12-09 05:29:24 +0000

    hy

---
+++ days2/d2.js
@@ -0,0 +1,8 @@
+let a=23;
+const b="abc";
+var c=78;
+d=88;
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
---
+++ days2/file2.js
@@ -0,0 +1,18 @@
+/*let a="abc";
+let b=" ";
+let c=null;
+let d;
+console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
+console.log(typeof(a));
+console.log(typeof(c));
+*/
+let score="abc";
+console.log(typeof(score));
+console.log(typeof score);
+let score2=Number(score);
+console.log(typeof(score2));
+console.log(typeof score2);
+
